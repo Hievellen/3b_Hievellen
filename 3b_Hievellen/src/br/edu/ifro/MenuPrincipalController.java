5e78638fa058370bfbaa7f43a66c3dcbf1fe55e1
01714922260 <01714922260@JIPA-LABIN03-31.ifro.local>
2018-05-04 16:48:22 -0400

    Calculadora

--- 3b_Hievellen/src/br/edu/ifro/MenuPrincipalController.java
+++ 3b_Hievellen/src/br/edu/ifro/MenuPrincipalController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.edu.ifro;
 
 import java.net.URL;
@@ -10,21 +6,26 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author 01714922260
- */
+
+
 public class MenuPrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNum1, txtNum2, txtResultado;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnSoma; 
+            
+    @FXML
+    private void soma(ActionEvent event) {
+        double num1 = Double.parseDouble(txtNum1.getText());
+        double num2 = Double.parseDouble(txtNum2.getText());
+        double resultado = (num1 + num2);
+        
+        txtResultado.setText(resultado.toString());
     }
     
     @Override
